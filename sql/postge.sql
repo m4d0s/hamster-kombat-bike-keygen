56a31b62481f49a265a7266e00ef0cd3a8beaa41
Smile Greenlight <60644076+M4dOS@users.noreply.github.com>
2024-08-08 15:16:00 +1000

    postgres script update

--- sql/postge.sql
+++ sql/postge.sql
@@ -1,78 +1,96 @@
--- Создание схемы
+-- DROP SCHEMA "hamster-kombat-keys";
+
 CREATE SCHEMA "hamster-kombat-keys" AUTHORIZATION postgres;
 
--- Создание последовательностей
+-- DROP SEQUENCE "hamster-kombat-keys".keys_id_seq;
+
 CREATE SEQUENCE "hamster-kombat-keys".keys_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1
-    NO CYCLE;
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE "hamster-kombat-keys".promo_id_seq;
 
 CREATE SEQUENCE "hamster-kombat-keys".promo_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1
-    NO CYCLE;
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE "hamster-kombat-keys".users_id_seq;
 
 CREATE SEQUENCE "hamster-kombat-keys".users_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1
-    NO CYCLE;
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;-- "hamster-kombat-keys".promo определение
 
--- Создание таблицы promo
+-- Drop table
+
+-- DROP TABLE "hamster-kombat-keys".promo;
+
 CREATE TABLE "hamster-kombat-keys".promo (
-    id int8 GENERATED ALWAYS AS IDENTITY(
-        INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE
-    ) NOT NULL,
-    "name" text NOT NULL,
-    "desc" text NOT NULL,
-    link text NOT NULL,
-    check_id int2 DEFAULT 0 NOT NULL,
-    CONSTRAINT promo_pk PRIMARY KEY (id),
-    CONSTRAINT unique_promo_link UNIQUE (link)
+	id int8 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	"name" text NOT NULL,
+	"desc" text NOT NULL,
+	link text NOT NULL,
+	check_id int2 DEFAULT 0 NOT NULL,
+	CONSTRAINT promo_pk PRIMARY KEY (id)
 );
 
--- Создание таблицы users
+
+-- "hamster-kombat-keys".users определение
+
+-- Drop table
+
+-- DROP TABLE "hamster-kombat-keys".users;
+
 CREATE TABLE "hamster-kombat-keys".users (
-    id int8 GENERATED ALWAYS AS IDENTITY(
-        INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE
-    ) NOT NULL,
-    tg_username text DEFAULT '-'::text NOT NULL,
-    tg_id int8 DEFAULT 0 NOT NULL,
-    ref_id int8 DEFAULT '-1'::integer NOT NULL,
-    "right" int2 DEFAULT 0 NOT NULL,
-    CONSTRAINT newtable_pk PRIMARY KEY (id),
-    CONSTRAINT unique_tg_id UNIQUE (tg_id)
+	id int8 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	tg_username text DEFAULT '-'::text NOT NULL,
+	tg_id int8 DEFAULT 0 NOT NULL,
+	ref_id int8 DEFAULT '-1'::integer NOT NULL,
+	"right" int2 DEFAULT 0 NOT NULL,
+	CONSTRAINT newtable_pk PRIMARY KEY (id),
+	CONSTRAINT users_unique UNIQUE (tg_id)
 );
 
--- Создание таблицы keys
+
+-- "hamster-kombat-keys"."keys" определение
+
+-- Drop table
+
+-- DROP TABLE "hamster-kombat-keys"."keys";
+
 CREATE TABLE "hamster-kombat-keys"."keys" (
-    id int8 GENERATED ALWAYS AS IDENTITY(
-        INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE
-    ) NOT NULL,
-    user_id int8 DEFAULT 0 NOT NULL,
-    "key" text NOT NULL,
-    "type" text DEFAULT 'KEY'::text NOT NULL,
-    "time" int8 DEFAULT 0 NULL,
-    CONSTRAINT keys_pk PRIMARY KEY (id),
-    CONSTRAINT unique_key UNIQUE ("key"),
-    CONSTRAINT keys_users_fk FOREIGN KEY (user_id) REFERENCES "hamster-kombat-keys".users(id) ON DELETE CASCADE ON UPDATE CASCADE
+	id int8 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE) NOT NULL,
+	user_id int8 DEFAULT 0 NOT NULL,
+	"key" text NOT NULL,
+	"type" text DEFAULT 'KEY'::text NOT NULL,
+	"time" int8 DEFAULT 0 NOT NULL,
+	used bool DEFAULT true NOT NULL,
+	CONSTRAINT keys_pk PRIMARY KEY (id),
+	CONSTRAINT keys_unique UNIQUE (key),
+	CONSTRAINT keys_users_fk FOREIGN KEY (user_id) REFERENCES "hamster-kombat-keys".users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- Создание таблицы subs
+
+-- "hamster-kombat-keys".subs определение
+
+-- Drop table
+
+-- DROP TABLE "hamster-kombat-keys".subs;
+
 CREATE TABLE "hamster-kombat-keys".subs (
-    id int8 NOT NULL,
-    user_id int8 NOT NULL,
-    promo_id int8 NULL,
-    CONSTRAINT subs_pk PRIMARY KEY (id),
-    CONSTRAINT subs_promo_fk FOREIGN KEY (promo_id) REFERENCES "hamster-kombat-keys".promo(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT subs_users_fk FOREIGN KEY (user_id) REFERENCES "hamster-kombat-keys".users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT unique_user_promo UNIQUE (user_id, promo_id)
+	id int8 NOT NULL,
+	user_id int8 NOT NULL,
+	promo_id int8 NULL,
+	CONSTRAINT subs_pk PRIMARY KEY (id),
+	CONSTRAINT subs_promo_fk FOREIGN KEY (promo_id) REFERENCES "hamster-kombat-keys".promo(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT subs_users_fk FOREIGN KEY (user_id) REFERENCES "hamster-kombat-keys".users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
