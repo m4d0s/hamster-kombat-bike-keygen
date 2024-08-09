659b0537e76c8b5722efaf0fc4a0f17016c7c64e
root <root@glossy-flower.aeza.network>
2024-08-09 20:07:23 +0000

    for linux

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Обновляем pip
 python3 -m pip install --upgrade pip
-pip install -r req.txt
-python3 telegram.py
+
+# Устанавливаем зависимости из файла req.txt
+pip3 install -r req.txt
+
+# Запускаем telegram.py в фоне
+python3 telegram.py &
+
+# Ждем 15 секунд
+sleep 15
+
+# Запускаем solo.py в фоне
+python3 solo.py &
+
